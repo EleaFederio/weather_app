a6cb5bab3c100e41e123ab5f2b60fbb4ed3cd901
EleazarFederio <eleazarfederio_@outlook.com>
2021-02-16 23:15:50 +0800

    dislplay location

--- App.js
+++ App.js
@@ -1,8 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    load()
+  }, [])
+
+  async function load(){
+    try {
+      let { status} = await Location.requestPermissionsAsync();
+      if(status !== 'granted'){
+        setErrorMessage('Access location is needed')
+        return 
+      }
+      const location = await Location.getCurrentPositionAsync();
+
+      const {latitude, longitude} = location.coords;
+
+      alert(`latitude: ${latitude} | longitude: ${longitude}`)
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
